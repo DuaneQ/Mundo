cac7171d6e7c8bb5ffa20725ce59c6b3704676c3
DuaneQ <DuaneQHodges@gmail.com>
2018-05-05 21:47:24 -0400

    added date picker and location for itinerary creator

---
+++ src/models/itinerary.ts
@@ -0,0 +1,5 @@
+export interface IItinerary{
+    date: Date;
+    location: string;
+    activities: string[];
+}
--- src/pages/itinerary/itinerary.ts
+++ src/pages/itinerary/itinerary.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, NgZone, OnInit } from '@angular/core';
 import { NavController, NavParams, ViewController } from 'ionic-angular';
+import { MapsAPILoader } from '@agm/core';
 
 /**
  * Generated class for the ItineraryPage page.
@@ -12,19 +13,41 @@ import { NavController, NavParams, ViewController } from 'ionic-angular';
   selector: 'page-itinerary',
   templateUrl: 'itinerary.html',
 })
-export class ItineraryPage {
+export class ItineraryPage implements OnInit{
+
+  startDate: string = new Date().toISOString();
+  endDate: string = new Date().toISOString();
+  destination: string;
+
+  @ViewChild('destin') public destinElement: ElementRef;
 
   constructor(private navCtrl: NavController, 
               private navParams: NavParams,
+              private mapsApiLoader: MapsAPILoader,
+              private ngZone: NgZone,
               private view: ViewController) {
   }
+
+  ngOnInit(){
+        this.mapsApiLoader.load().then(
+        () => {
+          let autocomplete = new google.maps.places.Autocomplete(this.destinElement.nativeElement, {types:['(cities)']});
+
+          autocomplete.addListener("place_changed", () => {
+            this.ngZone.run(() => {
+                let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+                if(place.geometry === undefined || place.geometry === null){
+                  return;
+              }
+                this.destination = place.formatted_address;
+              });
+            });
+          }
+        )
+    }
 
   closeModal(){
     this.view.dismiss();
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ItineraryPage');
-  }
-
 }
